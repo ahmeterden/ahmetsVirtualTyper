b04fe12e7979f9fecf58fc10c9c0468155183f6e
ahmet <ahmet@ahmeterden.com>
2019-02-28 13:27:42 +0300

    Add project files.

---
+++ ahmetsVirtualTyper/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace ahmetsVirtualTyper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbDelay = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbRate = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbPaste = new System.Windows.Forms.TextBox();
+            this.btn_begin = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbDelay
+            // 
+            this.tbDelay.Location = new System.Drawing.Point(109, 36);
+            this.tbDelay.Name = "tbDelay";
+            this.tbDelay.Size = new System.Drawing.Size(37, 20);
+            this.tbDelay.TabIndex = 0;
+            this.tbDelay.Text = "2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(152, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "seconds.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(276, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "This program types the things you pasted in the\r\ntextbox below. You can use it in" +
+    " RDP.";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Begin writing after";
+            // 
+            // tbRate
+            // 
+            this.tbRate.Enabled = false;
+            this.tbRate.Location = new System.Drawing.Point(109, 62);
+            this.tbRate.Name = "tbRate";
+            this.tbRate.Size = new System.Drawing.Size(37, 20);
+            this.tbRate.TabIndex = 4;
+            this.tbRate.Text = "5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(152, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "strokes/second.";
+            // 
+            // tbPaste
+            // 
+            this.tbPaste.Location = new System.Drawing.Point(10, 111);
+            this.tbPaste.Multiline = true;
+            this.tbPaste.Name = "tbPaste";
+            this.tbPaste.Size = new System.Drawing.Size(275, 72);
+            this.tbPaste.TabIndex = 6;
+            // 
+            // btn_begin
+            // 
+            this.btn_begin.Location = new System.Drawing.Point(10, 85);
+            this.btn_begin.Name = "btn_begin";
+            this.btn_begin.Size = new System.Drawing.Size(275, 23);
+            this.btn_begin.TabIndex = 7;
+            this.btn_begin.Text = "Start timer to write";
+            this.btn_begin.UseVisualStyleBackColor = true;
+            this.btn_begin.Click += new System.EventHandler(this.btn_begin_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Write at a rate of ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 195);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn_begin);
+            this.Controls.Add(this.tbPaste);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbRate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbDelay);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(50, 70);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Ahmet\'s Virtual Typer v2.0";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbDelay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbRate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbPaste;
+        private System.Windows.Forms.Button btn_begin;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ahmetsVirtualTyper/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsInput;
+
+namespace ahmetsVirtualTyper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_begin_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = Int32.Parse(tbDelay.Text) * 1000;
+            timer1.Enabled = true;
+        }
+		                     
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            InputSimulator myKb = new InputSimulator();
+            myKb.Keyboard.TextEntry(tbPaste.Text);
+
+			//old approach, before RDP typing
+
+            //foreach (char myChar in tbPaste.Text)
+            //{
+            //    string tempStr;
+
+            //    if(myChar == '+' || myChar == '^' || myChar == '%' || myChar == '~' || myChar == '(' || myChar == ')' || myChar == '{' || myChar == '}')
+            //    {
+            //        tempStr = "{" + myChar.ToString() + "}";
+            //    }
+            //    else
+            //    {
+            //        tempStr = myChar.ToString();
+            //    }
+
+            //    SendKeys.Send(tempStr);
+            //    Thread.Sleep(1000/Int32.Parse(tbDelay.Text));
+            //}
+
+            timer1.Enabled = false;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
